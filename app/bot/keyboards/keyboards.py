8713db3bed1de300b00ceab85e7c4c0c23f9ded7
Nadya Velikorodnyaya <n.velikorodny@gmail.com>
2025-06-09 16:34:51 +0300

    added keyboards

---
+++ app/bot/keyboards/keyboards.py
@@ -0,0 +1,37 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def get_lang_settings_kb(i18n: dict, locales: list[str], checked: str) -> InlineKeyboardMarkup:
+    buttons = []
+    for locale in sorted(locales):
+        if locale == "default":
+            continue
+        if locale == checked:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"🔘 {i18n.get(locale)}", callback_data=locale
+                    )
+                ]
+            )
+        else:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"⚪️ {i18n.get(locale)}", callback_data=locale
+                    )
+                ]
+            )
+    buttons.append(
+        [
+            InlineKeyboardButton(
+                text=i18n.get("cancel_lang_button_text"),
+                callback_data="cancel_lang_button_data"
+            ),
+            InlineKeyboardButton(
+                text=i18n.get("save_lang_button_text"),
+                callback_data="save_lang_button_data"
+            ),
+        ]
+    )
+    return InlineKeyboardMarkup(inline_keyboard=buttons)

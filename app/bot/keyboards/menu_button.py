2a74884c7996ddd7c4d2a4743c3bf2d12ca6cacf
Nadya Velikorodnyaya <n.velikorodny@gmail.com>
2025-06-09 16:40:10 +0300

    added main menu

---
+++ app/bot/keyboards/menu_button.py
@@ -0,0 +1,47 @@
+from aiogram.types import BotCommand
+from app.bot.enums.roles import UserRole
+
+
+def get_main_menu_commands(i18n: dict[str, str], role: UserRole):
+    if role == UserRole.USER:
+        return [
+            BotCommand(
+                command='/start',
+                description=i18n.get('/start_description')
+            ),
+            BotCommand(
+                command='/lang',
+                description=i18n.get('/lang_description')
+            ),
+            BotCommand(
+                command='/help',
+                description=i18n.get('/help_description')
+            ),
+        ]
+    elif role == UserRole.ADMIN:
+        return [
+            BotCommand(
+                command='/start',
+                description=i18n.get('/start_description')
+            ),
+            BotCommand(
+                command='/lang',
+                description=i18n.get('/lang_description')
+            ),
+            BotCommand(
+                command='/help',
+                description=i18n.get('/help_description')
+            ),
+            BotCommand(
+                command='/ban',
+                description=i18n.get('/ban_description')
+            ),
+            BotCommand(
+                command='/unban',
+                description=i18n.get('/unban_description')
+            ),
+            BotCommand(
+                command='/statistics',
+                description=i18n.get('/statistics_description')
+            ),
+        ]
